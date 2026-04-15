944b25972d103dcd26e9f43b97916776491fc0e5
Dylan Beattie <dylan@dylanbeattie.net>
2026-04-15 15:51:50 +0100

    Add Venues controller and CRUD views
    Configures the application to support MVC controllers and views, and adds a scaffolded VenuesController with associated Razor views for venue management. Also updates the shared navigation to include links for both Artists and Venues.

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IReportServerStatus>(new StatusReporter());
 
 var sqliteConnection = new SqliteConnection("Data Source=:memory:");
@@ -37,6 +38,8 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.MapGet("/status", (IReportServerStatus reporter)
 	=> reporter.GetStatus());
