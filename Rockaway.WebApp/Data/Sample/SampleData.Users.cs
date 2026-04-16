cae4275caf547763fc5a661820fd5a37246d931d
Dylan Beattie <dylan@dylanbeattie.net>
2026-04-16 14:20:34 +0100

    migration to add sample admin user when migrating database

--- Rockaway.WebApp/Data/Sample/SampleData.Users.cs
+++ Rockaway.WebApp/Data/Sample/SampleData.Users.cs
@@ -7,7 +7,6 @@ namespace Rockaway.WebApp.Data.Sample;
 public static partial class SampleData {
 	public static class Users {
 		static Users() {
-			var hasher = new PasswordHasher<IdentityUser>();
 			Admin = new() {
 				Id = "rockaway-sample-admin-user",
 				Email = "admin@rockaway.dev",
@@ -17,9 +16,10 @@ public static partial class SampleData {
 				LockoutEnabled = true,
 				EmailConfirmed = true,
 				PhoneNumberConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString()
+				SecurityStamp = "3c97071d-3e21-43a2-9d33-9d34bfdc362c",
+				ConcurrencyStamp = "0e259722-7dca-478c-a5c0-b67923aeea8c",
+				PasswordHash = "AQAAAAIAAYagAAAAEApY1qm1sD6qkAFm1wuBEV6+IdiTuGTqfmK6HWGPkG8MWRQ/fDljgEy39GRMp3VJ6g=="
 			};
-			Admin.PasswordHash = hasher.HashPassword(Admin, "p@ssw0rd");
 		}
 		public static IdentityUser Admin { get; }
 	}
